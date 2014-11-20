b5cf80fea419625b6dca11e3e68c2c509dde837a
Simon Hunt <shunt@singinghorsestudio.com>
2014-11-20 17:09:00 +0000

    new branch for old code

---
+++ jambi-client/public/js/editor.js
@@ -0,0 +1,12 @@
+// Editors functionality is in this file
+// 
+// The model represented is the editor only, no other functionality is in here
+
+
+var jmodel = function() {
+
+}
+
+jmodel.prototype.updateModel = function() {
+	var input = $('#jambiEditor').val();
+}
