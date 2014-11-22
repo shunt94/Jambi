9e249964f50390b579e899e0bbf25555d137cf44
Simon Hunt <shunt94@hotmail.co.uk>
2014-11-22 14:03:48 +0000

    Changed App Name in plist, Added new way of menubar implementation.. Much cleanergit add -u

--- jambi-client/public/js/jambi.js
+++ jambi-client/public/js/jambi.js
@@ -1,62 +1,72 @@
 var Jambi = function() {
+	
+	
+	Jambi.prototype.menuSetup = function() {
+		var jSetup = new jambiSetup();	
+		jSetup.jambiMenu.file.fileSave.click = function() {
+			jambi.saveFile();
+		}
+	}
+	
+	
+	Jambi.prototype.initCodeMirror = function() {
+	
 		
-}
-
-Jambi.prototype.init = function() {
-
-
-
-	// Create code mirror
-	var mixedMode = {
-	    name: "htmlmixed",
-	    scriptTypes: [{matches: /\/x-handlebars-template|\/x-mustache/i,
-	       mode: null},
-	      {matches: /(text|application)\/(x-)?vb(a|script)/i,
-	       mode: "vbscript"}]
-	  };
 	
-	var jambiEditor = CodeMirror(document.getElementById('jambi-editor'), {
-	  mode:  mixedMode,
-	  theme: "monokai",
-	  lineWrapping: true,
-	  lineNumbers: true,
-	  tabSize: 4,
-	  indentUnit: 4,
-	  indentWithTabs: true,
-	  autoCloseTags: true
-	});
+		
+		// Create code mirror
+		var mixedMode = {
+		    name: "htmlmixed",
+		    scriptTypes: [{matches: /\/x-handlebars-template|\/x-mustache/i,
+		       mode: null},
+		      {matches: /(text|application)\/(x-)?vb(a|script)/i,
+		       mode: "vbscript"}]
+		  };
+		
+		var jambiEditor = CodeMirror(document.getElementById('jambi-editor'), {
+		  mode:  mixedMode,
+		  theme: "monokai",
+		  lineWrapping: true,
+		  lineNumbers: true,
+		  tabSize: 4,
+		  indentUnit: 4,
+		  indentWithTabs: true,
+		  autoCloseTags: true
+		});
+		
+		jambiEditor.focus();
+	}
 	
-	jambiEditor.focus();
-}
+	Jambi.prototype.newFile = function() {
+		console.log("Adding new file");
+		
+		// Create new instance of Editor
+		
+	
+		
+		// Set Editor to visible and others to hidden
+		
+	
+		
+		// Add new file tab to top bar
+		
+		
+				
+	}
+	
+	
+	
+	Jambi.prototype.saveFile = function() {
+		var textToWrite = $('#jambiEditor').val();
+		var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+		var fileNameToSaveAs = "untitled.html";
+	
+		var downloadLink = document.createElement("a");
+		downloadLink.download = fileNameToSaveAs;
+		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+		downloadLink.click();
+	}
 
-Jambi.prototype.newFile = function() {
-	console.log("Adding new file");
-	
-	// Create new instance of Editor
-	
-
-	
-	// Set Editor to visible and others to hidden
-	
-
-	
-	// Add new file tab to top bar
-	
-	
-			
-}
-
-
-
-Jambi.prototype.saveFile = function() {
-	var textToWrite = $('#jambiEditor').val();
-	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-	var fileNameToSaveAs = "untitled.html";
-
-	var downloadLink = document.createElement("a");
-	downloadLink.download = fileNameToSaveAs;
-	downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-	downloadLink.click();
 }
 
 
--- jambi-client/public/js/jambiSetup.js
+++ jambi-client/public/js/jambiSetup.js
@@ -1,5 +1,7 @@
 var jambiSetup = function() {
-	
+
+
+
 	var gui = require('nw.gui');
 	var win = gui.Window.get();
 	var menuBar = new gui.Menu({ type: 'menubar' });
@@ -13,38 +15,109 @@ var jambiSetup = function() {
 	var fileSubmenu = new gui.Menu();
 	var fileNewSub = new gui.Menu();
 	
-	var file = {
-		fileNew: 		new gui.MenuItem({ label: 'New', submenu: fileNewSub }),
-		fileNewSubmenu: {
-			new_html: 		new gui.MenuItem({ label: 'HTML' }),
-			new_css:		new gui.MenuItem({ label: 'CSS' }),
-			new_js:			new gui.MenuItem({ label: 'JS' })
+	// View Menus 
+	var viewSubmenu = new gui.Menu();
+	
+	// Text Menus
+	var textSubmenu = new gui.Menu();
+	
+	// Insert Menus
+	var insertSubmenu = new gui.Menu();
+	
+	// Instas Menus 
+	var instasSubmenu = new gui.Menu();
+	
+	// Version Control Menus
+	var vcSubmenu = new gui.Menu();
+	
+	
+	
+	// Data structure to hold the menubar -- used to access menuitems later
+	var jambiMenu = {
+		file: {
+			fileNew: 			new gui.MenuItem({ label: 'New', submenu: fileNewSub }),
+			fileNewSubmenu: [
+				{key: 'new_html', 		value: new gui.MenuItem({ label: 'Html' })},
+				{key: 'new_css',		value: new gui.MenuItem({ label: 'Css' })},
+				{key: 'new_js',			value: new gui.MenuItem({ label: 'Js' })},
+				{key: 'new_scss',		value: new gui.MenuItem({ label: 'Scss' })},
+				{key: 'new_less',		value: new gui.MenuItem({ label: 'Less' })},
+				{key: 'new_sass',		value: new gui.MenuItem({ label: 'Sass' })},
+				{key: 'new_python',		value: new gui.MenuItem({ label: 'Python' })}
+			],
+			fileNewProject:		new gui.MenuItem({ label: 'New Project' }),
+			fileOpen:			new gui.MenuItem({ label: 'Open', key: "o", modifiers: "cmd" }),
+			fileOpenRecent:		new gui.MenuItem({ label: 'Open Recent' }),
+			fileClose:			new gui.MenuItem({ label: 'Close' }),
+			fileCloseAll:		new gui.MenuItem({ label: 'Close All' }),
+			fileSave:			new gui.MenuItem({ label: 'Save', key: "s", modifiers: "cmd" }),
+			fileSaveAs:			new gui.MenuItem({ label: 'Save As...', key: "s",modifiers: "cmd-shift" })
 		},
-		fileNewProject:		new gui.MenuItem({ label: 'New Project' })
+		view: {
+			
+		},
+		text: {
+			
+		},
+		insert: {
+			
+		},
+		instas: {
+			
+		},
+		vc: {
+			
+		}
 	}
 	
 	
-	fileSubmenu.append(file.fileNew);
+
 	
-	fileNewSub.append(file.fileNewSubmenu.new_html);
-	fileNewSub.append(file.fileNewSubmenu.new_css);
-	fileNewSub.append(file.fileNewSubmenu.new_js);
 	
-	fileSubmenu.append(file.fileNewProject);
+	// Generate The File Menu
+	var generateFileMenu = (function() {
+		// File --> New Menu Adding
+		for(var x = 0; x < jambiMenu.file.fileNewSubmenu.length; x++) {
+			fileNewSub.append(jambiMenu.file.fileNewSubmenu[x].value);
+		}
 	
-	fileSubmenu.append(new gui.MenuItem({ type: 'separator' }));
+		// Build Menu
+		fileSubmenu.append(jambiMenu.file.fileNew);
+		fileSubmenu.append(jambiMenu.file.fileNewProject);
+		fileSubmenu.append(new gui.MenuItem({ type: 'separator' }));
+		fileSubmenu.append(jambiMenu.file.fileOpen);
+		fileSubmenu.append(jambiMenu.file.fileOpenRecent);
+		fileSubmenu.append(new gui.MenuItem({ type: 'separator' }));
+		fileSubmenu.append(jambiMenu.file.fileClose);
+		fileSubmenu.append(jambiMenu.file.fileCloseAll);
+		fileSubmenu.append(new gui.MenuItem({ type: 'separator' }));
+		fileSubmenu.append(jambiMenu.file.fileSave);
+		fileSubmenu.append(jambiMenu.file.fileSaveAs);
+	})();
 	
+
 	
 	addTopMenu(fileSubmenu, "File", 1);
+	addTopMenu(viewSubmenu, "View", 3);
+	addTopMenu(textSubmenu, "Text", 4);
+	addTopMenu(insertSubmenu, "Insert", 5);
+	addTopMenu(instasSubmenu, "Instas", 6);
+	addTopMenu(vcSubmenu, "Version Control", 7);
+	win.menu.append(new gui.MenuItem({ label: 'Help', submenu: new gui.Menu()}));
 	
+
 	
 	function addTopMenu(subMenu, nameLabel, pos) {
 		menuBar.insert(new gui.MenuItem({ label: nameLabel, submenu: subMenu}), pos);
 	} 
 	
 	
+		
+
+	
 
 	return {
 		gui: gui,
+		jambiMenu: jambiMenu
 	}	
 }
