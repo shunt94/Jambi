4fe46bf92081a8c04c949319c5ef53d301d5108e
Simon Hunt <shunt94@hotmail.co.uk>
2014-11-23 11:43:33 +0000

    Added Testing Suit and updated package.json

---
+++ jambi-client/test/test.js
@@ -0,0 +1,11 @@
+var assert = require("assert")
+
+describe('TestLabel1', function(){
+	it('Should pass', function(done) {
+		done();
+	});
+	
+	it('should fail', function(done) {
+		done();
+	});
+});
