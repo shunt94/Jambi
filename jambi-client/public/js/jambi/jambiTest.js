ad788a51d607bc8ae68ef8dbd950fe1e0ccb645a
Simon Hunt <shunt94@hotmail.co.uk>
2015-05-09 18:33:09 +0100

    more comments

--- jambi-client/public/js/jambi/jambiTest.js
+++ jambi-client/public/js/jambi/jambiTest.js
@@ -10,19 +10,21 @@ var JambiTest = function (htmlDoc) {
     var results = [];
 
     /*
-        Method:
-        Purpose:
+        Method: describe
+        Purpose: run the describe tests
     */
     JambiTest.prototype.describe = function(label, tests){
         tests();
     };
 
     /*
-        Method:
-        Purpose:
+        Method: should
+        Purpose: run the should tests
     */
     JambiTest.prototype.should = function(label, test) {
+        // increment the test counter
         testCounter++;
+        // send the done function as a parameter to the test
         var done = function(err) {
             if (err) {
                 that.fail(label);
@@ -32,43 +34,46 @@ var JambiTest = function (htmlDoc) {
                 that.pass(label);
             }
         };
-
+        // run the test with the done function
         test(done);
     };
 
     /*
-        Method:
-        Purpose:
+        Method: pass
+        Purpose: print results that passed the test
     */
     JambiTest.prototype.pass = function(label) {
         that.printResults(label, true);
     };
 
     /*
-        Method:
-        Purpose:
+        Method: fail
+        Purpose: print the results that failed
     */
     JambiTest.prototype.fail = function(label) {
         that.printResults(label, false);
     };
 
     /*
-        Method:
-        Purpose:
+        Method: printResults
+        Purpose: print results
     */
     JambiTest.prototype.printResults = function(label, passed) {
         var print;
+        // if passed print a tick, else print a cross
         if(passed) {
             print = '<span class="test-should"> It Should ' + label + ' <i class="fa fa-check test-pass"></i></span><br>';
         } else {
             print = '<span class="test-should"> It Should' + label + ' <i class="fa fa-times test-fail"></i></span><br>';
         }
+
+        // append test
         htmlDoc.append(print);
     };
 
     /*
-        Method:
-        Purpose:
+        Method: end
+        Purpose: append the tests and the end
     */
     JambiTest.prototype.end = function (){
         setTimeout(function(){
